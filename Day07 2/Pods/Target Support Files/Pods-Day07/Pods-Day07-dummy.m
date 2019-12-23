9505a9afca632df631c79422c799ff22dee220a0
vpelivan <pelivan.v.1988@gmail.com>
2019-12-23 21:17:14 +0200

    Swift pool 42

--- Day07 2/Pods/Target Support Files/Pods-Day07/Pods-Day07-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Day07 : NSObject
-@end
-@implementation PodsDummy_Pods_Day07
-@end
--- Day07 2/Pods/Target Support Files/RecastAI/RecastAI-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_RecastAI : NSObject
-@end
-@implementation PodsDummy_RecastAI
-@end
